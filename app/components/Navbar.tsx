b4b1992bcf2182e91960957f16ae07dd6e24fd61
piterjb <piter.jb0817@gmail.com>
2025-04-30 05:45:58 +0900

    feat: login remove

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -50,45 +50,8 @@ export default function Navbar({ activePage }: { activePage: 'login'|'dashboard'
         ))}
       </div>
 
-      {/* Dropdown button */}
       <div className="relative">
-        <button 
-          onClick={() => setDropdownOpen(!dropdownOpen)} 
-          className="bg-navy-700 text-white px-4 py-2 rounded-md pixel-font flex items-center"
-        >
-          <Image 
-            src="/images/solana-logo.svg" 
-            alt="Solana" 
-            width={20} 
-            height={20} 
-          />
-          <span className="truncate max-w-[150px] pl-2">{walletAddress}</span>
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            className="h-5 w-5 ml-1" 
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-          </svg>
-        </button>
-
-        {dropdownOpen && (
-          <div className="absolute right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg z-10">
-            <button 
-              onClick={handleCopy} 
-              className="block w-full text-left px-4 py-2 text-md text-bold text-white hover:bg-gray-700 cursor-pointer"
-            >
-              Copy Address
-            </button>
-            <button 
-              onClick={handleLogout} 
-              className="block w-full text-left px-4 py-2 text-md text-bold text-blue-500 hover:bg-gray-700 cursor-pointer"
-            >
-              Log Out
-            </button>
-          </div>
-        )}
+        
       </div>
     </nav>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,11 @@
 'use client';
 import Image from 'next/image';
 import SocialFooter from './components/SocialFooter';
-import PhantomLoginButton from './components/PhantomLoginButton';
+import DashboardPage from './dashboard/page';
 
 export default function LoginPage() {
 
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-800 to-gray-900">
-            
-      <div className="flex-grow flex flex-col items-center justify-center p-4">
-        <div className="w-full max-w-md mx-auto flex flex-col items-center">
-          <div className="mb-6 flex items-center">
-            <Image 
-              src="/images/binoculars.svg" 
-              alt="Whale Watcher Logo" 
-              width={32} 
-              height={32} 
-            />
-            <h1 className="pixel-font text-2xl font-bold ml-2 text-white">WHALE WATCHER</h1>
-          </div>
-          
-          <h2 className="pixel-font text-md text-center mb-10 text-white">TRACK BIG MOVES ON SOLANA IN REAL TIME</h2>
-          
-          <div className="w-full space-y-4 text-center">
-            <PhantomLoginButton />
-          </div>
-        </div>
-      </div>
-      
-      <div className="py-8">
-        <SocialFooter />
-      </div>
-    </div>
+    <DashboardPage/>
   );
 }
