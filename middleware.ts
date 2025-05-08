5a4925ab1272f5e69453e53672b5d8db159c5e56
piterjb <piter.jb0817@gmail.com>
2025-05-08 17:47:03 +0900

    feat:

--- middleware.ts
+++ middleware.ts
@@ -18,14 +18,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images (your static image folder)
-     */
     '/((?!api|_next/static|_next/image|favicon.ico|images).*)',
   ],
 };
