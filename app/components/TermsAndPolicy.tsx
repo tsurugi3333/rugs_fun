bef2bac10b2994282fc1449cb5754e3ed2d1fda0
piterjb <piter.jb0817@gmail.com>
2025-05-08 13:39:55 +0900

    feat:remove password page

--- app/components/TermsAndPolicy.tsx
+++ app/components/TermsAndPolicy.tsx
@@ -600,7 +600,7 @@ const TermsAndPolicyPage = () => {
         requested; when receipt is electronically confirmed, if transmitted by facsimile or e-mail; 
         or the day after it is sent, if sent for next day delivery by recognized overnight delivery 
         service.</p>
-        <p className="mx-5 mb-4">Advised by Michael Shelton of Maven Agency LLC, ZZ Ventures LLC, Charles Cui, Mission Street Ventures LLC and Social Flare Agency LLC</p>
+        <p className="mx-5 mb-4">Serving as an advisor to Combs Brothers Production LLC is Maven Agency LLC and Michael Shelton, Mission Street Ventures, Brannan Street Ventures, and by and through Reed Smith LLP</p>
       </div>
     </div>
   );
